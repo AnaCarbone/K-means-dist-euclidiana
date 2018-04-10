1da5ba37ed6efcd6b991efef5354b1a69719b489
AnaCarbone <anabcarbone@gmail.com>
2018-04-09 21:14:39 -0300

    alterações: cond de parada e zerar prototipos

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,59 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 	// args[0] = nome do arquivo
 	// args[1] = k grupos
 	// args[2] = taxa de erro
-	public static void main(String [] args)
-	{
-		boolean bomResultado = false;
-		int iteracoes = 0;
+	// args[3] = linhas
+	// args[4] = colunas
+	public static void main(String[] args) {
+		boolean bomResultado = false; // controla o fato de algoritmo já ter alcançado um bom resultado ou não
+		int iteracoes = 0; 
 		double jcmAtual = 0;
-		
-		// ver como vai passar essas coisas nome, k e erro
-		KMeans kMeans = new KMeans(args[0], Integer.parseInt(args[1]), Double.parseDouble(args[2]));
-		kMeans.definirPrototipos(); // defini prototipos randomicamente
-		
-		// iteracoes comecam aqui
-		while(!bomResultado)
-		{
+		double [][] prototiposAnterior;	
+		double condicaoParada = 0;
+
+		// inicialização do objeto kmeans
+		KMeansDouble kMeans = new KMeansDouble(args[0], Integer.parseInt(args[1]), Double.parseDouble(args[2]),
+				Integer.parseInt(args[3]), Integer.parseInt(args[4]));
+		prototiposAnterior = kMeans.definirPrototipos(); // defini prototipos randomicamente
+
+
+		// iteracoes comecam aqui 
+		while (!bomResultado || iteracoes<100) {
 			iteracoes++;
 			kMeans.definirDistanciasEuclidianas();
 			kMeans.clustering(); // atualiza matriz de particoes
-			jcmAtual = kMeans.calcularJCM();
-			if(iteracoes == 1)
-			{
-				kMeans.setJCM(jcmAtual);
+			jcmAtual = kMeans.calcularJCM(); // calcular o jcm - o qual deseja-se minimizar para otimização
+			kMeans.redefinirPrototipos(); // atualizar prototipos
+			
+			//testar condição de parada
+			condicaoParada = kMeans.diferencaPrototipos(prototiposAnterior);
+			if(condicaoParada<kMeans.getTaxaErro()){
+				bomResultado = true;
 			}
-			else
-			{
-				double diferencaJCM = kMeans.getJCM() - jcmAtual;
-				if(diferencaJCM < kMeans.getTaxaErro())
-				{
-					//NAO PRECISA DOS DOIS
-					bomResultado = true;
-					break;
-				}
-				else
-				{
-					kMeans.setJCM(jcmAtual); // atualiza o jcm para o último
-					kMeans.redefinirPrototipos(); // atualizar prototipos
-				}
-			}
+			System.out.println("CARREGADO TUN TUN TUN :D");
 		}
 		
+		
+		// escrever em um arquivo a matriz de particao final para analise dos grupos formados
+		int[][] resp = kMeans.getMatrizParticao();
+		try {
+			BufferedWriter br = new BufferedWriter(new FileWriter("resp.csv"));
+			for(int i = 0; i < Integer.parseInt(args[1]); i++){
+				for(int j = 0; j < Integer.parseInt(args[3]); j++){
+					br.append(resp[i][j] + ",");
+				}
+				br.newLine();
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+
 	}
+
 }
